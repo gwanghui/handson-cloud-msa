8d39d4a2f967013a62b5ddd7e0ccc77f1fa03b22
ueong <dnfod@me.com>
2020-10-29 15:11:08 +0900

    circuit breaker 실습

--- eshop-adservice/src/main/java/com/samsungsds/eshop/ad/AdController.java
+++ eshop-adservice/src/main/java/com/samsungsds/eshop/ad/AdController.java
@@ -13,14 +13,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 @RestController
 public class AdController {
     private Logger logger = LoggerFactory.getLogger(AdController.class);
-    // private static final ImmutableListMultimap<String, Ad> adsMap = createAdsMap();
     private final AdRepository adRepository;
     private static final Random random = new Random();
     private static final int MAX_ADS_TO_SERVE = 2;
@@ -31,22 +31,33 @@ public class AdController {
 
     @GetMapping
     @ResponseBody
+    @CircuitBreaker(name = "ads-circuit-breaker", fallbackMethod = "getAdsFallback")
     public ResponseEntity<List<Ad>> getRandomAds() {
         logger.info("getRandomAds");
         List<Ad> ads = new ArrayList<>(MAX_ADS_TO_SERVE);
         List<Ad> allAds = Lists.newArrayList(adRepository.findAll());
-        for(int i = 0; i < MAX_ADS_TO_SERVE; i++) {
+        for (int i = 0; i < MAX_ADS_TO_SERVE; i++) {
             ads.add(Iterables.get(allAds, random.nextInt(allAds.size())));
         }
         logger.info(ads.toString());
         return ResponseEntity.ok(ads);
     }
 
-    @GetMapping(value="/{categories}")
+    @GetMapping(value = "/{categories}")
     @ResponseBody
+    @CircuitBreaker(name = "ads-circuit-breaker", fallbackMethod = "getAdsFallback")
     public ResponseEntity<List<Ad>> getAdsByCategory(@PathVariable String[] categories) {
         logger.info("getAdsByCategory {}", Arrays.toString(categories));
         List<Ad> ads = Lists.newArrayList(adRepository.findByCategoryIn(categories));
+        logger.info(ads.toString());
+        return ResponseEntity.ok(ads);
+    }
+
+    // fallback method
+    private ResponseEntity<List<Ad>> getAdsFallback(Exception e) {
+        logger.info("Fallback : returning a static ad");
+        List<Ad> ads = new ArrayList<>();
+        ads.add(adRepository.findById(1).orElse(null));
         logger.info(ads.toString());
         return ResponseEntity.ok(ads);
     }
